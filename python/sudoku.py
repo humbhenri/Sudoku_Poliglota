f28a288b1cd7085566f552845a932d04fbf6e4db
Humberto Pinheiro <humbhenri@gmail.com>
2016-01-11 20:20:07 -0200

    make tests more readable

--- python/sudoku.py
+++ python/sudoku.py
@@ -5,16 +5,17 @@ import sys
 import time
 import os
 
-BOARD_SIZE=9
+BOARD_SIZE = 9
+
 
 def load_bar(step, total_steps, resolution, width):
-    if total_steps/resolution == 0 :
+    if total_steps / resolution == 0:
         return
-    if step % (total_steps/resolution) != 0:
+    if step % (total_steps / resolution) != 0:
         return
-    ratio = step/float(total_steps)
+    ratio = step / float(total_steps)
     count = int(ratio * width)
-    print('%3d%% ['.format(int(ratio*100)))
+    print('%3d%% ['.format(int(ratio * 100)))
     for x in range(0, count):
         sys.stdout.write('=')
     for x in range(count, width):
@@ -22,11 +23,12 @@ def load_bar(step, total_steps, resolution, width):
     sys.stdout.write(']\r')
     sys.stdout.flush()
 
+
 def next_empty(sudoku):
     for i in range(0, len(sudoku)):
         for j in range(0, len(sudoku[i])):
             if sudoku[i][j] == 0:
-                return i,j
+                return i, j
     return None
 
 
@@ -36,10 +38,10 @@ def can_put(sudoku, x, y, val):
             return False
         if sudoku[x][i] == val:
             return False
-    sq_x = x-(x%3)
-    sq_y = y-(y%3)
-    for i in range(sq_x, sq_x+3):
-        for j in range(sq_y, sq_y+3):
+    sq_x = x - (x % 3)
+    sq_y = y - (y % 3)
+    for i in range(sq_x, sq_x + 3):
+        for j in range(sq_y, sq_y + 3):
             if sudoku[i][j] == val:
                 return False
     return True
@@ -55,9 +57,9 @@ def solve(sudoku):
             sudoku[x][y] = i
             new_sudoku = solve(sudoku)
             if next_empty(new_sudoku) is None:
-                return new_sudoku #found solution
+                return new_sudoku  #found solution
 
-    sudoku[x][y] = 0 #solution not found, backtrack
+    sudoku[x][y] = 0  #solution not found, backtrack
     return sudoku
 
 
@@ -66,8 +68,8 @@ def to_str(sudoku):
 
 
 def from_str(data):
-    rows = [data[i:i+BOARD_SIZE] for i in range(0, len(data), BOARD_SIZE)]
-    return [list(map(int,list(row))) for row in rows]
+    rows = [data[i:i + BOARD_SIZE] for i in range(0, len(data), BOARD_SIZE)]
+    return [list(map(int, row)) for row in rows]
 
 
 def process(input, output):
@@ -79,26 +81,24 @@ def process(input, output):
     sudokus = data.splitlines()
     total_sudokus = len(sudokus)
     solved_sudokus = 0
-    result = []
     for line in sudokus:
         sudoku = from_str(line)
         sudoku = solve(sudoku)
-        result.append(to_str(sudoku))
+        output.write(to_str(sudoku) + '\n')
         solved_sudokus += 1
         load_bar(solved_sudokus, total_sudokus, 100, 50)
     after = time.time()
 
-    # write solutions to file
-    output.write('\n\n'.join(result))
-
     # present results
-    print('--Elapsed %f ms' % ((after-before)*1000))
+    print('--Elapsed %f ms' % ((after - before) * 1000))
+
 
 if __name__ == '__main__':
     try:
         filename = sys.argv[1]
         with io.open(filename, 'r') as input:
-            with io.open('solved_' + os.path.basename(filename), 'w') as output:
+            with io.open('solved_' + os.path.basename(filename),
+                         'w') as output:
                 process(input, output)
     except IndexError:
         print('Insert file input')
--- python/sudoku_test.py
+++ python/sudoku_test.py
@@ -1,9 +1,8 @@
 import unittest
 from .sudoku import *
+from copy import deepcopy
 
-class TestSudoku(unittest.TestCase):
-    def test_from_str(self):
-        self.assertEqual([
+SUDOKU_SAMPLE = [
             [2, 0, 0, 0, 0, 0, 0, 6, 0],
             [0, 0, 0, 0, 7, 5, 0, 3, 0],
             [0, 4, 8, 0, 9, 0, 1, 0, 0],
@@ -12,11 +11,11 @@ class TestSudoku(unittest.TestCase):
             [0, 0, 0, 0, 0, 8, 0, 0, 0],
             [0, 0, 1, 0, 2, 0, 5, 7, 0],
             [0, 8, 0, 7, 3, 0, 0, 0, 0],
-            [0, 9, 0, 0, 0, 0, 0, 0, 4]],
-                         from_str("200000060000075030048090100000300000300010009000008000001020570080730000090000004"))
+            [0, 9, 0, 0, 0, 0, 0, 0, 4]]
 
-    def test_to_str(self):
-        self.assertEqual("""2 0 0 0 0 0 0 6 0
+SUDOKU_SAMPLE_LINE = "200000060000075030048090100000300000300010009000008000001020570080730000090000004"
+
+SUDOKU_SAMPLE_FORMATED = """2 0 0 0 0 0 0 6 0
 0 0 0 0 7 5 0 3 0
 0 4 8 0 9 0 1 0 0
 0 0 0 3 0 0 0 0 0
@@ -24,48 +23,20 @@ class TestSudoku(unittest.TestCase):
 0 0 0 0 0 8 0 0 0
 0 0 1 0 2 0 5 7 0
 0 8 0 7 3 0 0 0 0
-0 9 0 0 0 0 0 0 4""",
-                         to_str(
-                             [[2, 0, 0, 0, 0, 0, 0, 6, 0],
-                              [0, 0, 0, 0, 7, 5, 0, 3, 0],
-                              [0, 4, 8, 0, 9, 0, 1, 0, 0],
-                              [0, 0, 0, 3, 0, 0, 0, 0, 0],
-                              [3, 0, 0, 0, 1, 0, 0, 0, 9],
-                              [0, 0, 0, 0, 0, 8, 0, 0, 0],
-                              [0, 0, 1, 0, 2, 0, 5, 7, 0],
-                              [0, 8, 0, 7, 3, 0, 0, 0, 0],
-                              [0, 9, 0, 0, 0, 0, 0, 0, 4]]))
+0 9 0 0 0 0 0 0 4
+"""
 
-    def test_solve(self):
-        self.assertEqual([[2, 7, 3, 4, 8, 1, 9, 6, 5],
-                          [9, 1, 6, 2, 7, 5, 4, 3, 8],
-                          [5, 4, 8, 6, 9, 3, 1, 2, 7],
-                          [8, 5, 9, 3, 4, 7, 6, 1, 2],
-                          [3, 6, 7, 5, 1, 2, 8, 4, 9],
-                          [1, 2, 4, 9, 6, 8, 7, 5, 3],
-                          [4, 3, 1, 8, 2, 9, 5, 7, 6],
-                          [6, 8, 5, 7, 3, 4, 2, 9, 1],
-                          [7, 9, 2, 1, 5, 6, 3, 8, 4]],
-                         solve([[2, 0, 0, 0, 0, 0, 0, 6, 0],
-                                [0, 0, 0, 0, 7, 5, 0, 3, 0],
-                                [0, 4, 8, 0, 9, 0, 1, 0, 0],
-                                [0, 0, 0, 3, 0, 0, 0, 0, 0],
-                                [3, 0, 0, 0, 1, 0, 0, 0, 9],
-                                [0, 0, 0, 0, 0, 8, 0, 0, 0],
-                                [0, 0, 1, 0, 2, 0, 5, 7, 0],
-                                [0, 8, 0, 7, 3, 0, 0, 0, 0],
-                                [0, 9, 0, 0, 0, 0, 0, 0, 4]]))
+SUDOKU_SOLUTION = [[2, 7, 3, 4, 8, 1, 9, 6, 5],
+                   [9, 1, 6, 2, 7, 5, 4, 3, 8],
+                   [5, 4, 8, 6, 9, 3, 1, 2, 7],
+                   [8, 5, 9, 3, 4, 7, 6, 1, 2],
+                   [3, 6, 7, 5, 1, 2, 8, 4, 9],
+                   [1, 2, 4, 9, 6, 8, 7, 5, 3],
+                   [4, 3, 1, 8, 2, 9, 5, 7, 6],
+                   [6, 8, 5, 7, 3, 4, 2, 9, 1],
+                   [7, 9, 2, 1, 5, 6, 3, 8, 4]]
 
-    def test_process(self):
-        input = io.StringIO()
-        input.write(u'200000060000075030048090100000300000300010009000008000001020570080730000090000004')
-        input.seek(0)
-        output = io.StringIO()
-        process(input, output)
-        results = output.getvalue()
-        input.close()
-        output.close()
-        self.assertEqual(u"""2 7 3 4 8 1 9 6 5
+SUDOKU_SOLUTION_FORMATED = u"""2 7 3 4 8 1 9 6 5
 9 1 6 2 7 5 4 3 8
 5 4 8 6 9 3 1 2 7
 8 5 9 3 4 7 6 1 2
@@ -73,7 +44,28 @@ class TestSudoku(unittest.TestCase):
 1 2 4 9 6 8 7 5 3
 4 3 1 8 2 9 5 7 6
 6 8 5 7 3 4 2 9 1
-7 9 2 1 5 6 3 8 4""", results)
+7 9 2 1 5 6 3 8 4"""
+
+class TestSudoku(unittest.TestCase):
+    def test_from_str(self):
+        self.assertEqual(SUDOKU_SAMPLE, from_str(SUDOKU_SAMPLE_LINE))
+
+    def test_to_str(self):
+        self.assertEqual(SUDOKU_SAMPLE_FORMATED.strip(), to_str(SUDOKU_SAMPLE).strip())
+
+    def test_solve(self):
+        self.assertEqual(SUDOKU_SOLUTION, solve(deepcopy(SUDOKU_SAMPLE)))
+
+    def test_process(self):
+        input = io.StringIO()
+        input.write(SUDOKU_SAMPLE_LINE)
+        input.seek(0)
+        output = io.StringIO()
+        process(input, output)
+        results = output.getvalue()
+        input.close()
+        output.close()
+        self.assertEqual(SUDOKU_SOLUTION_FORMATED.strip(), results.strip())
 
 if __name__ == '__main__':
     unittest.main()
