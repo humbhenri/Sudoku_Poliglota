4f5640e65e5c79c866134e895dc012ee48616af1
Humberto Pinheiro <humbhenri@gmail.com>
2016-01-14 16:39:48 -0200

    Board struct

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,24 +1,29 @@
 
 const ROW_SIZE: usize = 9;
 
-fn from_str(sudoku: &str) -> [[u32; ROW_SIZE]; ROW_SIZE] {
-    let mut board = [[0; ROW_SIZE]; ROW_SIZE];
-    let mut row = 0;
-    let mut col = 0;
-    for c in sudoku.chars() {
-        board[row][col] = c.to_digit(10).unwrap();
-        col += 1;
-        if col == ROW_SIZE {
-            col = 0;
-            row += 1;
+#[derive(Debug)]
+struct Board([[u32; ROW_SIZE]; ROW_SIZE]);
+
+impl Board {
+    fn from_str(sudoku: &str) -> Board {
+        let mut board = [[0; ROW_SIZE]; ROW_SIZE];
+        let mut row = 0;
+        let mut col = 0;
+        for c in sudoku.chars() {
+            board[row][col] = c.to_digit(10).unwrap();
+            col += 1;
+            if col == ROW_SIZE {
+                col = 0;
+                row += 1;
+            }
         }
+        Board(board)
     }
-    board
 }
 
 fn main() {
     let sudoku_example = "200000060000075030048090100000300000300010009000008000001020570080730000090000004";
-    let mut board: [[u32; ROW_SIZE]; ROW_SIZE] = from_str(sudoku_example);
+    let board = Board::from_str(sudoku_example);
     println!("{:?}", board)
 }
 
