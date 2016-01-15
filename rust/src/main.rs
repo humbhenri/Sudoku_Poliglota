e090de2d7b04a32707580290c51bcafa11cfb488
Humberto Pinheiro <humbhenri@gmail.com>
2016-01-15 09:24:26 -0200

    add formatted display for Sudoku struct

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,30 +1,43 @@
+use std::fmt;
 
 const ROW_SIZE: usize = 9;
 
 #[derive(Debug)]
-struct Board([[u32; ROW_SIZE]; ROW_SIZE]);
+struct Sudoku([[u32; ROW_SIZE]; ROW_SIZE]);
 
-impl Board {
-    fn from_str(sudoku: &str) -> Board {
+impl Sudoku {
+    fn from_str(sudoku: &str) -> Sudoku {
         let mut board = [[0; ROW_SIZE]; ROW_SIZE];
-        let mut row = 0;
-        let mut col = 0;
-        for c in sudoku.chars() {
-            board[row][col] = c.to_digit(10).unwrap();
-            col += 1;
-            if col == ROW_SIZE {
-                col = 0;
-                row += 1;
-            }
+    let mut row = 0;
+    let mut col = 0;
+    for c in sudoku.chars() {
+        board[row][col] = c.to_digit(10).unwrap();
+        col += 1;
+        if col == ROW_SIZE {
+            col = 0;
+            row += 1;
         }
-        Board(board)
+    }
+    Sudoku(board)
+    }
+}
+
+impl fmt::Display for Sudoku {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut s: String = String::new();
+        for row in self.0.iter() {
+            s.push_str(
+                &row.iter().map(|&x| x.to_string()).collect::<Vec<String>>().join(" "));
+            s.push_str("\n");
+        }
+        write!(f, "{}", s)
     }
 }
 
 fn main() {
     let sudoku_example = "200000060000075030048090100000300000300010009000008000001020570080730000090000004";
-    let board = Board::from_str(sudoku_example);
-    println!("{:?}", board)
+    let board = Sudoku::from_str(sudoku_example);
+    println!("{}", board)
 }
 
 // #[cfg(test)]
