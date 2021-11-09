b2131e945563d00dc55376a75d2687dc8971fc53
Humberto Pinheiro <humbhenri@gmail.com>
2021-11-09 12:22:50 -0300

    run each benchmark in parallel

--- benchmark.js
+++ benchmark.js
@@ -8,69 +8,70 @@ const make = 'make';
 const make_run = 'make run';
 const make_clean = 'make clean';
 
-const projects = ['c', 
-    'clisp', 
-//    'clojureapp', 
-    'cpp', 
+const projects = ['c',
+    'clisp',
+    // 'clojureapp',
+    'cpp',
     'csharp',
-    'd', 
-//    'elixir', 
-    'go', 
-    'haskell', 
-//    'io', 
-//    'janet', 
-    'java', 
+    'd',
+//    'elixir',
+    'go',
+    'haskell',
+//    'io',
+//    'janet',
+    'java',
     'javascript',
-    'lua', 
-    'nim', 
-//    'python', 
-    'rust', 
+    'lua',
+    'nim',
+    'python',
+    'rust',
 //    'typescriptapp'
     'zig'
 ];
 
-async function execShell(cmd) {
-    const { stdout, stderr } = await exec(cmd)
+async function execShell(cmd, cwd) {
+    const { stdout, stderr } = await exec(cmd, { cwd })
     if (stderr) {
         throw stderr
     }
     return stdout
 }
 
+const working_dir = process.cwd()
+
+async function runMake(project) {
+    try {
+        const cwd = `${working_dir}/${project}`
+        console.log(`Running make at project ${project}`)
+        console.log(await execShell(make_clean, cwd))
+        console.log(await execShell(make, cwd))
+    } catch (error) {
+        console.log(`Error at project ${project}`)
+        console.error(error)
+    }
+}
+
+async function solve(project) {
+    try {
+        const cwd = `${working_dir}/${project}`
+        console.log(`Running benchmark of project ${project}`)
+        const t0 = process.hrtime()
+        await execShell(make_run, cwd)
+        const [ sec, nanosec ] = process.hrtime(t0)
+        const elapsed_time_ms = (sec * 1e9 + nanosec) / 1000000
+        console.log(`Solve finished for project ${project}: ${elapsed_time_ms.toFixed(3)} ms`)
+        return { project: project, elapsed_time_ms: elapsed_time_ms.toFixed(3) }
+    } catch (error) {
+        console.log(`Error at project ${project}`)
+        console.error(error)
+    }
+}
+
 async function run() {
-    const working_dir = process.cwd()
     console.log('Running make run on all sub projects')
-    for (const p of projects) {
-        try {
-            process.chdir(p)
-            console.log(`Running make at project ${p}`)
-            console.log(await execShell(make_clean))
-            console.log(await execShell(make))
-            process.chdir(working_dir)
-        } catch (error) {
-            console.log(`Error at project ${p}`)
-            console.error(error)
-            process.chdir(working_dir)
-        } 
-    }
+    await Promise.all(projects.map(project => runMake(project)))
     console.log('Benchmarking ...')
-    const results = []
-    for (const p of projects) {
-        try {
-            process.chdir(p)
-            console.log(`Running benchmark of project ${p}`)
-            const t0 = process.hrtime()
-            await execShell(make_run)
-            const [ sec, nanosec ] = process.hrtime(t0)
-            const elapsed_time_ms = (sec * 1e9 + nanosec) / 1000000
-            results.push({ project: p, elapsed_time_ms: elapsed_time_ms.toFixed(3) })
-            process.chdir(working_dir)
-        } catch (error) {
-            console.log(`Error at project ${p}`)
-            console.error(error)
-            process.chdir(working_dir)
-        } 
-    }
+    const results = await Promise.all(projects.map(project => solve(project)))
     results.sort((a, b) => a.elapsed_time_ms - b.elapsed_time_ms)
     console.table(results)
 }
